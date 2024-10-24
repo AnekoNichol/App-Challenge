2a55bee8fc84041e24789edf7b29697c9dbca8b2
AnekoNichol <148379494+AnekoNichol@users.noreply.github.com>
2024-10-24 15:21:02 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import tensorflow as tf
+import numpy as np
+
+def model_prediction(test_image):
+    model = tf.keras.models.load_model("trained_model.keras")
+    image = tf.keras.preprocessing.image.load_img(test_image,target_size=(128,128))
+    input_arr = tf.keras.preprocessing.image.img_to_array(image)
+    input_arr = np.array([input_arr]) 
+    predictions = model.predict(input_arr)
+    result_index = np.argmax(predictions) 
+    return result_index
